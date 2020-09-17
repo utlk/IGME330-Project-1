9a7dc029f3056fdce24914ee02a01dcdab65943b
Tyler Lynch <shimmmygorth@gmail.com>
2020-09-17 18:01:12 -0400

    Initial Work on the sine wave page finished

---
+++ src/talLIB.js
@@ -0,0 +1,20 @@
+"use strict"
+const gravity = 10; //Gravity value in pixels/sec
+const tick = 
+(function()  {
+    let canvasWidth = ctx.canvas.width;
+    let canvasHeight = ctx.canvas.height;
+   
+    //Draws a sinWave
+    function drawSin(ctx){
+        let scale = 20;
+
+        ctx.save();
+        ctx.beginPath()
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "red";
+
+        let x = 0, y= 0;
+    }
+    
+})();
